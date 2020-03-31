511c2315afc21cb24a7dd3d3e9e0949f4297e3cb
cegijon <53779413+cegijon@users.noreply.github.com>
2020-03-31 16:31:43 +1100

    recorriendo arrays de alumnas en React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TarjetaDeAlumna from './TarjetaDeAlumna';
+
+
+const alumnas = ["Celi", "Flor", "Guada", "Sofi"]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        alumnas.map((alumna, i) => <TarjetaDeAlumna key={i} nombreDeAlumna={alumna} />
+
+        )
+      }
     </div>
   );
 }
